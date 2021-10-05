b7633cfc28b67d20a823804d7f5fd53b3f212a8e
ualali <74206178+ualali@users.noreply.github.com>
2021-10-05 14:43:42 -0500

    Refactor trial app

--- project/app/trial/app.py
+++ project/app/trial/app.py
@@ -1,73 +1,58 @@
-from flask import Flask, render_template, request, jsonify
-
+from flask import Flask, jsonify
 from jaeger_client import Config
-from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
 from opentelemetry import trace
-from opentelemetry.exporter import jaeger
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
     ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
 )
+
+import logging
+import requests
+
+
+def initialize_tracer():
+    """
+    Initializes an instance of the Jaeger tracer.
+    """
+
+    logging.getLogger("").handlers = []
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+    open_tracing_config = {"sampler": {"type": "const", "param": 1}, "logging": True}
+    tracer_config = Config(open_tracing_config, service_name="trial")
+
+    return tracer_config.initialize_tracer()
+
+
+app = Flask(__name__)
 
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(ConsoleSpanExporter())
+    BatchSpanProcessor(ConsoleSpanExporter())
 )
-
-app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+tracer = initialize_tracer()
 
 
-#config = Config(
-#        config={},
-#        service_name='your-app-name',
-#        validate=True,
-#        metrics_factory=PrometheusMetricsFactory(service_name_label='your-app-name')
-#)
-#tracer = config.initialize_tracer()
-
-def init_tracer(service):
-    logging.getLogger('').handlers = []
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'logging': True,
-        },
-        service_name=service,
-    )
-
-    # this call also sets opentracing.tracer
-    return config.initialize_tracer()
-
-tracer = init_tracer('first-service')
-
-@app.route('/')
+@app.route("/")
 def homepage():
-    return render_template("main.html")
-    with tracer.start_span('get-python-jobs') as span:
+    with tracer.start_span("get-python-jobs") as span:
         homepages = []
-        res = requests.get('https://jobs.github.com/positions.json?description=python')
-        span.set_tag('first-tag', len(res.json()))
+        res = requests.get("https://jobs.github.com/positions.json?description=python")
+        span.set_tag("first-tag", len(res.json()))
+
         for result in res.json():
             try:
-                homepages.append(requests.get(result['company_url']))
+                homepages.append(requests.get(result["company_url"]))
             except:
-                return "Unable to get site for %s" % result['company']
-        
-
+                return "Unable to get site for %s" % result["company"]
 
     return jsonify(homepages)
 
+
 if __name__ == "__main__":
-    app.run(debug=True,)
+    app.run(debug=True)
